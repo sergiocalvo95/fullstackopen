48535eff7a8a54d11974a583ae93d5a6228baa60
Sergio Calvo <scalvogarcia95@gmail.com>
2024-10-19 13:36:55 +0200

    1.3 - 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,25 +9,27 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  const parts = props.parts;
+  
   return (
     <div>
-    <p>
-    {props.part1} {props.exercises1}
-  </p>
-  <p>
-    {props.part2} {props.exercises2}
-  </p>
-  <p>
-    {props.part3} {props.exercises3}
-  </p>
-  </div>
-  )
+      {parts.map((part, index) =>(
+        <p key={index}>
+        {part.name} {part.exercises} 
+        </p>
+      ))}
+    </div>
+  );
 }
 
 const Total = (props) => {
+  const parts = props.parts;
+  let total = 0
+  parts.forEach(part=> total+=part.exercises)
+
   return (
     <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {total}</p>
     </div>
   )
 
@@ -37,19 +39,29 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}></Header>
-      <Content part1 = {part1} part2={part2} part3 = {part3} exercices1={exercises1} exercices2={exercises2} exercices3={exercises3}></Content>
-      <Total exercices1={exercises1} exercices2={exercises2} exercices3={exercises3}></Total>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
